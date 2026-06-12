d28459270d513262e50db79a84173b0d058598ec
ItsOlegDm <itsolegdm@gmail.com>
2026-06-12 20:24:54 +0000

    fix broken server config

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'https://mc.foxgirls.ovh'),
   app: {
     keys: env.array('APP_KEYS'),
   },
